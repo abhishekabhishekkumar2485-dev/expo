fa8a1e4fc9b4ec2863c31ce527c374e5bdadec3c
HubertBer <bernackihubert03@gmail.com>
2026-07-10 12:49:33 +0200

    remove debug

--- packages/expo-type-information/__tests__/DebugModule.swift
+++
@@ -1,8 +0,0 @@
-public class ContactsNextModule: Module {
-  private static let contactsDidChangeEvent = "contactsDidChange"
-  Events(ContactsNextModule.contactsDidChangeEvent)
-}
-
-public class SomeClass {
-    private static let someOtherVar = 214
-}
